dc9c3452b509c05f355915c39756310acea74353
alraeesiclearing-sudo <alraeesiclearing-sudo@gmail.com>
2026-05-22 14:45:33 +0000

    Add Express backend server with API routes

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// API Routes
+app.get('/du/common/myaccount/backend-routine/json/landingPageData.json', (req, res) => {
+  const dataPath = path.join(__dirname, 'du/common/myaccount/backend-routine/json/landingPageData.json');
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      return res.json({ error: true, message: 'Data not found' });
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.get('/du/common/myaccount/backend-routine/error.json', (req, res) => {
+  const errorPath = path.join(__dirname, 'du/common/myaccount/backend-routine/error.json');
+  fs.readFile(errorPath, 'utf8', (err, data) => {
+    if (err) {
+      return res.json({ error: true });
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// Servlet routes
+app.get('/servlet/myaccount/en/mya-quick-pay-payment.html', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Quick Pay</title>
+    </head>
+    <body>
+      <input type="hidden" name="_authkey_" value="test_auth_key_12345">
+      <script>
+        console.log('Quick Pay Payment Page Loaded');
+      </script>
+    </body>
+    </html>
+  `);
+});
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html for root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Serve ar/quick-pay.html for /ar/quick-pay
+app.get('/ar/quick-pay', (req, res) => {
+  res.sendFile(path.join(__dirname, 'ar/quick-pay.html'));
+});
+
+// Catch all - serve index.html for React routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'ar/quick-pay.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
